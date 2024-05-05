ff9ea912f732c514f47da03bef0fd986084b6e8c
justinli007 <justinli2432@gmail.com>
2024-05-05 15:42:20 -0400

    Add html page generator from md file

--- src/main.py
+++ src/main.py
@@ -1,16 +1,62 @@
+import sys
 import os
 import shutil
 from  textnode import TextNode
+from markdown import markdown_to_html_node
 
 def main():
-    path = "./"
+    root = "./"
     src = "static"
     dst = "public"
+    indexMarkdownPath = "content"
     ignore = ['_', '.']
 
-    clearDir(path, dst)
-    createDir(path, dst)
-    copyFilesRecursive(os.path.join(path, src), os.path.join(path, dst), ignore)
+    clearDir(root, dst)
+    createDir(root, dst)
+    copyFilesRecursive(os.path.join(root, src), os.path.join(root, dst), ignore)
+
+    generate_page(os.path.join(root, "content", "index.md"), os.path.join(root,
+        "template.html"), os.path.join(root, dst, "index.html"))
+
+def generate_page(srcPath, templatePath, dstPath):
+    print(f"Generating page from {srcPath} to {dstPath} using {templatePath}.") 
+
+    mdFile = None
+    templateHtml = None
+    title = None
+    contentBody = None
+    htmlPage = None
+
+    try:
+        mdFile = readFile(srcPath)
+        templateHtml = readFile(templatePath)
+    except Exception as ex:
+        print(str(ex))
+        print("Exiting...")
+        sys.exit(1)
+
+    try:
+        title = extract_title(mdFile)
+        contentBody = mdFile.split(title, 1)[-1]
+    except Exception as ex:
+        print(str(ex))
+        print("Exiting...")
+        sys.exit(1)
+    
+    htmlPage = markdown_to_html_node(contentBody)
+    templateHtml = templateHtml.replace("{{ Title }}", title.lstrip("#").lstrip())
+    templateHtml = templateHtml.replace("{{ Content }}", repr(htmlPage))
+
+    saveFile(dstPath, templateHtml)
+    return 
+
+def extract_title(markdown):
+    if markdown == None or len(markdown) <= 0:
+        return ""
+    title = markdown.lstrip().split("\n", 1)[0]
+    if not title.startswith("#"):
+        raise Exception("Markdown title not found.")
+    return title
 
 def clearDir(path, targetDir):
     if targetDir in os.listdir(path):
@@ -21,9 +67,11 @@ def createDir(path, newDir, permissions=0o777):
     try:
         os.mkdir(os.path.join(path, newDir), permissions)
         print(f"Directory created: {os.path.join(path, newDir)}")
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        print(str(ex))
         print(f"Path does not exist: {path}")
-    except FileExistsError:
+    except FileExistsError as ex:
+        print(str(ex))
         print(f"Directory already exists: {newDir}")
 
 def RmTreeErrorHandler(func, path, excinfo):
@@ -44,5 +92,20 @@ def copyFilesRecursive(path, dst, ignore=None):
             if not os.path.exists(os.path.join(dst, entry)):
                 createDir(dst, entry)
             copyFilesRecursive(tempPath, os.path.join(dst, entry), ignore)
+
+def readFile(path):
+    content = None
+    with open(path, "r", encoding="utf-8") as f:
+        content = f.read()
+    if not f.closed:
+        f.close()
+    return content
+
+def saveFile(path, content):
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(content)
+    if not f.closed:
+        f.close()
+    return
 
 main()
